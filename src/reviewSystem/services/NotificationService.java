dba5d8ef89cf49af13274ffe7776032e5239634b
Jacob Erasmus <j.g.erasmus@outlook.com>
2026-05-05 16:29:13 +0200

    notifcation alts

--- src/reviewSystem/services/NotificationService.java
+++ src/reviewSystem/services/NotificationService.java
@@ -1,5 +1,16 @@
 package reviewSystem.services;
 
 public class NotificationService {
-    
+    // [alt accepted] EvaluationManager calls NoficationService notifyAcceptance()
+    public void notifyAcceptance(Submission submission) {
+        System.out.println("Notification: Acceptance for: " + submission.getId());
+    }
+    // [alt rejected] EvaluationManager calls NotificationService notifyRejection()
+    public void notifyRejection(Submission submission) {
+        System.out.println("Notification: Rejection for: " + submission.getId());
+    }
+    // [alt revision] EvaluationManager calls NotificationService notifyRevision()
+    public void notifyRevision(Submission submission) {
+        System.out.println("Notification: Revision needed for: " + submission.getId());
+    }
 }
