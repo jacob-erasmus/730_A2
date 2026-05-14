9dc5e0ab5436247905dde3ce0aa381f160d71669
Jacob Erasmus <j.g.erasmus@outlook.com>
2026-05-14 04:52:27 +0200

    UI added to optimised verison.

--- Optimised/src/reviewSystemO/ui/UI.java
+++ Optimised/src/reviewSystemO/ui/UI.java
@@ -1,5 +1,22 @@
 package reviewSystemO.ui;
 
+import reviewSystemO.controllers.SubmissionController;
+
+/**
+ * Tracable interactions:
+ * Researcher -> UI submitResearchOutput(data)
+ * UI -> SubmissionController submit(data)
+ * No optimisation, no change.
+ */
 public class UI {
-    
+    private SubmissionController submissionController;
+
+    public UI(SubmissionController controller) {
+        this.submissionController = controller;
+    }
+    // Receives submission from researcher, passes to submision controller.
+    public void submitResearchOutput(Object data) {
+        // UI -> SubmissionController submit(data)
+        submissionController.submit(data);
+    }
 }
