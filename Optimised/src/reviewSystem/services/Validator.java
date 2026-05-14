612c7faa24e9f69389c9f7c7bc17a4184105b820
Jacob Erasmus <j.g.erasmus@outlook.com>
2026-05-14 02:37:44 +0200

    Validator.java optimised

--- Optimised/src/reviewSystem/services/Validator.java
+++ Optimised/src/reviewSystem/services/Validator.java
@@ -1,5 +1,20 @@
 package reviewSystem.services;
 
+/**
+ * Tracable interactions:
+ * SubmissionController -> Validator validate(data)
+ * Validator -> SubmissionController returns boolean: isValid (optimisation is the return of boolean instead of string)
+ */
 public class Validator {
-    
+    public Validator() {
+    }
+    // Diagram: SubmissionController -> Validator validate(data)
+    // Returns: boolean isValid.
+    public boolean validate(Object data) {
+        if (data == null) {
+            return false;
+        }
+        // check format
+        return true;
+    }
 }
